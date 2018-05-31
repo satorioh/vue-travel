639a9d03ae63c187c62ff60dca4782dd42ab9313
satorioh <wangbinxp@gmail.com>
2018-05-31 14:40:51 +0800

    add weekend

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,12 +2,12 @@
   <div class="recommend">
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom">
-        <img class="item-img" src="http://img1.qunarzz.com/sight/p0/1603/85/85aa0c000578a01e90.water.jpg_200x200_0ccb4aa8.jpg" alt="">
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="">
         <div class="item-info">
-          <p class="item-info-title">拙政园</p>
-          <p class="item-info-desc">江南园林甲天下，苏州园林甲江南</p>
-          <button>查看详情</button>
+          <p class="item-info-title">{{item.title}}</p>
+          <p class="item-info-desc">{{item.desc}}</p>
+          <button class="item-info-button">查看详情</button>
         </div>
       </li>
     </ul>
@@ -16,11 +16,42 @@
 
 <script>
 export default {
-  name: 'HomeRecommend'
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '01',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/85/85aa0c000578a01e90.water.jpg_200x200_0ccb4aa8.jpg',
+          title: '拙政园',
+          desc: '江南园林甲天下，苏州园林甲江南'
+        },
+        {
+          id: '02',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1805/6f/6fd1ba315b60e430a3.water.jpg_200x200_16184926.jpg',
+          title: '周庄',
+          desc: '江南园林甲天下，苏州园林甲江南'
+        },
+        {
+          id: '03',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/201405/14/3dfad2bcd30c40e91a15f8b7296d86a6.jpg_200x200_265db173.jpg',
+          title: '同里古镇',
+          desc: '江南园林甲天下，苏州园林甲江南'
+        },
+        {
+          id: '04',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/27/6640b07ce9a3dd7d9e37743ff58c1c3f.water.jpg_200x200_d7cd9982.jpg',
+          title: '木渎古镇',
+          desc: '江南园林甲天下，苏州园林甲江南'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
   .title
     line-height : .8rem
     background : #eee
@@ -36,7 +67,20 @@ export default {
     .item-info
       flex : 1
       padding: .1rem
+      min-width : 0
       .item-info-title
         line-height : .54rem
         font-size : .32rem
+        ellipsis()
+      .item-info-desc
+        line-height : .4rem
+        color: #ccc
+        ellipsis()
+      .item-info-button
+        line-height : .44rem
+        margin-top : .16rem
+        background : #ff9300
+        padding: 0 .2rem
+        border-radius : .06rem
+        color: #fff
 </style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="recommend">
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-info-title">{{item.title}}</p>
+          <p class="item-info-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '01',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/8a/d412db9fcd44dd.jpg_r_640x214_86f303a2.jpg',
+          title: '拙政园',
+          desc: '江南园林甲天下，苏州园林甲江南'
+        },
+        {
+          id: '02',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/70/b2f8425ce486ed.jpg_r_640x214_5db9de57.jpg',
+          title: '周庄',
+          desc: '江南园林甲天下，苏州园林甲江南'
+        },
+        {
+          id: '03',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1507/ab/1c456b9cbffc31.jpg_r_640x214_1c2552de.jpg',
+          title: '同里古镇',
+          desc: '江南园林甲天下，苏州园林甲江南'
+        },
+        {
+          id: '04',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1505/3e/18b666031b22e7.jpg_r_640x214_864dce2f.jpg',
+          title: '木渎古镇',
+          desc: '江南园林甲天下，苏州园林甲江南'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/mixins.styl"
+  .title
+    line-height : .8rem
+    background : #eee
+    text-indent : .2rem
+  .item-img-wrapper
+    overflow : hidden
+    height : 0
+    padding-bottom : 33.9%
+    .item-img
+      width : 100%
+  .item-info
+    padding: .2rem
+    min-width : 0
+    .item-info-title
+      line-height : .54rem
+      font-size : .32rem
+      ellipsis()
+    .item-info-desc
+      line-height : .4rem
+      color: #ccc
+      ellipsis()
+</style>
