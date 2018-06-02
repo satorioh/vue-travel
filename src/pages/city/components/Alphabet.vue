b15860a767e6d99f4fac9388ecf896b033ac200d
Roubin <wangbinxp@sina.com>
2018-06-02 21:41:49 +0800

    add city-components

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,index) of cities" :key="index">{{index}}</li>
+    <li class="item" v-for="item of letters" :key="item" :ref="item" @click="handlerClick" @touchstart="handlerTouchStart" @touchmove="handlerTouchMove" @touchend="handlerTouchEnd">{{item}}</li>
   </ul>
 </template>
 
@@ -9,6 +9,51 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    handlerClick (e) {
+      this.$emit('change', e.target.textContent)
+    },
+    handlerTouchStart () {
+      this.touchStatus = true
+    },
+    handlerTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 78.4
+          const index = Math.floor((touchY - this.startY) / 20)
+          console.log(index)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handlerTouchEnd () {
+      this.touchStatus = false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
   }
 }
 </script>
